02c1a3760d00ecd9f5135e31219c0bab66630094
bichnga1845 <ngattb234111e@st.uel.edu.vn>
2024-09-30 13:58:49 +0700

    Update MainWindowEx.py

--- MainWindowEx.py
+++ MainWindowEx.py
@@ -9,15 +9,7 @@ customers = {}
 class MainWindowEx(Ui_MainWindow):
     def __init__(self):
         super().__init__()  # Khởi tạo lớp cha
-        self.MainWindow = None  # Khởi tạo MainWindow
-        self.id_input = None  # Tham chiếu cho id_input
-        self.name_input = None  # Tham chiếu cho name_input
-        self.email_input = None  # Tham chiếu cho email_input
-        self.customer_list = None  # Tham chiếu cho customer_list
-        self.add_button = None  # Tham chiếu cho add_button
-        self.edit_button = None  # Tham chiếu cho edit_button
-        self.delete_button = None  # Tham chiếu cho delete_button
-        self.reset_button = None  # Tham chiếu cho reset_button
+
 
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
