18f4b7b34e5b47d5690b8a5a13f66197a491dba7
bichnga1845 <ngattb234111e@st.uel.edu.vn>
2024-09-30 15:27:29 +0700

    Summary
    Thêm liên kết cho button

--- MainWindowEx.py
+++ MainWindowEx.py
@@ -2,10 +2,18 @@ from MainWindow import Ui_MainWindow
 
 
 class MainWindowEx(Ui_MainWindow):
+    customers = {}
     def __init__(self):
         pass
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
+        self.add_button.clicked.connect(self.add_customer)
+        self.edit_button.clicked.connect(self.edit_customer)
+        self.delete_button.clicked.connect(self.delete_customer)
+        self.reset_button.clicked.connect(self.reset_fields)
+        self.customer_list.itemClicked.connect(self.load_selected_customer)
+        self.load_customers()
+
         self.MainWindow=MainWindow
     def show(self):
         self.MainWindow.show()
