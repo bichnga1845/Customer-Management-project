a434cda44590d05f8c96500acd3a70b21ce390a1
bichnga1845 <ngattb234111e@st.uel.edu.vn>
2024-09-30 15:56:00 +0700

    Update MainWindowEx.py

--- MainWindowEx.py
+++ MainWindowEx.py
@@ -14,31 +14,31 @@ class MainWindowEx(Ui_MainWindow):
         self.reset_button.clicked.connect(self.reset_fields)
         self.customer_list.itemClicked.connect(self.load_selected_customer)
         self.load_customers()
-            #Hien thong tin khach hang
-        def load_customers(self):
-            self.customer_list.clear()
-            for id, details in customers.items():
-                self.customer_list.addItem(f"ID: {id} | Name: {details['name']} | Email: {details['email']}")
-
-            # ADD CUSTOMER
-
-        def add_customer(self):
-            id = self.id_input.text().strip()
-            name = self.name_input.text().strip()
-            email = self.email_input.text().strip()
-
-            if id in customers:
-                QMessageBox.critical(self, "Lỗi", "Mã khách hàng đã tồn tại!")
-                return
-
-            if not id or not name or not email:
-                QMessageBox.critical(self, "Lỗi", "Vui lòng nhập đầy đủ thông tin!")
-                return
-
-            customers[id] = {'name': name, 'email': email}
-            QMessageBox.information(self, "Thành công", "Khách hàng đã được thêm!")
-            self.load_customers()
-
         self.MainWindow=MainWindow
     def show(self):
         self.MainWindow.show()
+
+        # Hien thong tin khach hang
+    def load_customers(self):
+        self.customer_list.clear()
+        for id, details in customers.items():
+            self.customer_list.addItem(f"ID: {id} | Name: {details['name']} | Email: {details['email']}")
+
+            # ADD CUSTOMER
+
+    def add_customer(self):
+        id = self.id_input.text().strip()
+        name = self.name_input.text().strip()
+        email = self.email_input.text().strip()
+
+        if id in customers:
+            QMessageBox.critical(self, "Lỗi", "Mã khách hàng đã tồn tại!")
+            return
+
+        if not id or not name or not email:
+            QMessageBox.critical(self, "Lỗi", "Vui lòng nhập đầy đủ thông tin!")
+            return
+
+        customers[id] = {'name': name, 'email': email}
+        QMessageBox.information(self, "Thành công", "Khách hàng đã được thêm!")
+        self.load_customers()
